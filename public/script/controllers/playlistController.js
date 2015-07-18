8fe8c8ff3d4bde65435d32c1482a5c1fb441aa1b
Henry <hlewis@uchicago.edu>
2015-07-18 11:53:59 -0500

    fixed up

--- public/script/controllers/playlistController.js
+++ public/script/controllers/playlistController.js
@@ -1,13 +1,23 @@
 
 
-function PlaylistController($scope){
+function PlaylistController($scope, $timeout){
   $scope.playlist = playlist;
   $scope.results = []
   $scope.query = "";
+  var timeoutPromise;
+
   $scope.Search = function() {
     search.search($scope.query, {}, function (ret) {
       console.log(ret);
       $scope.results = ret.results;
     });
+  }
+  $scope.triggerSearch = function (enter) {
+  	$timeout.cancel(timeoutPromise);
+  	if (enter) {
+  		$scope.Search;
+  	} else {
+	  	timeoutPromise = $timeout($scope.Search, 500);
+  	}
   }
 }
