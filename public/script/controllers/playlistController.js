4cc9c3cc44d3268e4842f2d7900f882f5a48fa64
Henry <hlewis@uchicago.edu>
2015-08-11 19:35:45 -0500

    fullscreen

--- public/script/controllers/playlistController.js
+++ public/script/controllers/playlistController.js
@@ -165,6 +165,27 @@ function PlaylistController($scope, $timeout, $q, $window, $document){
     	}
     }
 
+    $scope.toggleFullScreen = function () {
+		if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+			(!document.mozFullScreen && !document.webkitIsFullScreen)) {
+			if (document.documentElement.requestFullScreen) {  
+				document.documentElement.requestFullScreen();  
+			} else if (document.documentElement.mozRequestFullScreen) {  
+				document.documentElement.mozRequestFullScreen();  
+			} else if (document.documentElement.webkitRequestFullScreen) {  
+				document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+			}  
+		} else {  
+			if (document.cancelFullScreen) {  
+				document.cancelFullScreen();  
+			} else if (document.mozCancelFullScreen) {  
+				document.mozCancelFullScreen();  
+			} else if (document.webkitCancelFullScreen) {  
+				document.webkitCancelFullScreen();  
+			}  
+		}  
+	}
+
 	angular.element($window).bind("scroll", function() {
 	    var windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
 	    var body = document.body, html = document.documentElement;
